8ddb7ca1d6b9f0779da2f89d0a11a3da01083ba7
Steph DGenius <stephanetse@gmail.com>
2022-09-14 15:26:55 +0100

    0.1.0+alpha

--- gh-build.sh
+++ gh-build.sh
@@ -1,19 +1,16 @@
 #! /bin/sh
 
-# Build process
-
 apt-get --yes update
 apt-get --yes install wget equivs curl git
 
 deps=$(sed -e '/^#.*$/d; /^$/d; /^\s*$/d' package/dependencies | paste -sd ,)
-git_commit=$(git rev-parse --short HEAD)
 
 printf >configuration "%s\n" \
 	"Section: admin" \
 	"Priority: required" \
 	"Homepage: https://kaytime.github.io" \
 	"Package: system-layer-core" \
-	"Version: $git_commit" \
+	"Version: 0.1.0+alpha" \
 	"Maintainer: Stephane Tsana <stephanetse@gmail.com>" \
 	"Depends: $deps" \
 	"Architecture: all" \
